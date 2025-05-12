d65746c99bdb7b0d410d63839593baf1b2d2524d
josefina neely <joseneelyg@gmail.com>
2025-05-12 16:22:40 +0000

    comenzando

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -5,14 +5,17 @@ import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
+
+
+
 // index.css'
 import '../styles/index.css'
 
-// components
-import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+
+
+
+ReactDOM.render(<Home />, document.querySelector('#app'))
+
+
+
